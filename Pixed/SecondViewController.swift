629f64415a292ae2441d00f3eb9622594d92e01e
nishanth28 <pnishanth28@gmail.com>
2017-01-23 06:11:01 -0500

    Initial Commit

---
+++ Pixed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pixed
+//
+//  Created by Nishanth P on 1/23/17.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
